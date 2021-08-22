7e8c98e7fce2924256a249f61f2b0be2a30d0598
Jeremy Garrell <garrellj101@gmail.com>
2021-08-22 15:38:20 -0400

    update file naming. update system setup to add appending bashrc and profile

--- linux-setup/setup-system.sh
+++ linux-setup/setup-system.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-# This linux setup is for Ubuntu 18.04 Bionic Beaver
+# This linux setup is for Ubuntu 20.04 Focal Fossa
 
 HEAD="
 !----- "
 TAIL=" -----!
 "
 
+echo "$HEAD Installing curl $TAIL"
+sudo apt install curl
+
 echo "$HEAD Installing zsh $TAIL"
 sudo apt install zsh
 
 echo "$HEAD Setting zsh to default, to enable it you must log out $TAIL"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "$HEAD Installing python2 $TAIL"
-sudo apt install python
-
-echo "$HEAD Installing git $TAIL"
-sudo apt install git
 
 git config --global user.email "garrellj101@gmail.com"
 git config --global user.name "Jeremy Garrell"
@@ -31,12 +28,23 @@ cd ~/.vim/color-schemes
 cp -r colors ..
 
 sudo apt install vim
+sudo apt install vim-gtk3
 
 echo "$HEAD Installing my custom dot files $TAIL"
-[ -d ~/dotfiles/jeremy ] || git clone https://github.com/garrelj1/dump.git ~/dotfiles/jeremy
-[ -d ~/dotfiles/jeremy ] && cd ~/dotfiles/jeremy; git pull
+[ -d ~/.dotfiles] || git clone https://github.com/garrelj1/dump.git ~/.dotfiles
+[ -d ~/.dotfiles] && cd ~/.dotfiles; git pull
 
 echo "$HEAD Updating .vimrc $TAIL"
-cp ~/dotfiles/jeremy/.vimrc ~/
+cp ~/.dotfiles/linux-setup/.vimrc ~/
 
 echo "$HEAD Appending .profile $TAIL"
+cat ~/.dotfiles/linux-setup/.profile >> ~/.profile
+
+echo "$HEAD Appending .bashrc $TAIL"
+cat ~/.dotfiles/linux-setup/.bashrc >> ~/.bashrc
+
+echo "$HEAD Installing python2 $TAIL"
+sudo apt install python
+
+echo "$HEAD Installing Node JS $TAIL"
+sudo snap install node --classic
