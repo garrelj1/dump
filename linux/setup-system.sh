e4159ca3850cbe40fbf3ecba0d1e405cf9cc3bea
Jeremy Garrell <garrellj101@gmail.com>
2023-07-13 14:16:41 -0400

    Update ubuntu system setup script

--- linux/setup-system.sh
+++ linux/setup-system.sh
@@ -6,27 +6,31 @@ HEAD="
 TAIL=" -----!
 "
 
+# Before running this you need to setup an ssh key and add it to github
+# ssh-keygen -t ed25519 -C "your_email@example.com"
+
 echo "$HEAD Creating workspace $TAIL"
 mkdir ~/workspace
 
 echo "$HEAD Installing packages $TAIL"
 sudo apt install build-essential dkms
 
-echo "$HEAD Installing curl $TAIL"
-sudo apt install curl
+echo "$HEAD Installing curl and wget $TAIL"
+sudo apt install curl wget
 
-echo "$HEAD Installing zsh $TAIL"
-sudo apt install zsh
+echo "$HEAD Installing git$TAIL"
+sudo apt install git
 
-echo "$HEAD Setting zsh to default, to enable it you must log out $TAIL"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "$HEAD Setting oh-my-bash to default, to enable it you must log out $TAIL"
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
 
 git config --global user.email "garrellj101@gmail.com"
 git config --global user.name "Jeremy Garrell"
 
 echo "$HEAD Installing VIM and dependencies $TAIL"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/autoload -p
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mv plug.vim ~/.vim/autoload
 
 sudo apt install vim
 sudo apt install vim-gtk3
@@ -42,6 +46,9 @@ cat ~/.dotfiles/linux-setup/.profile >> ~/.profile
 
 echo "$HEAD Appending .bashrc $TAIL"
 cat ~/.dotfiles/linux-setup/.bashrc >> ~/.bashrc
+
+echo "$HEAD Installing other packages"
+sudo apt install gthumb
 
 echo "$HEAD Installing python2 $TAIL"
 sudo apt install python
